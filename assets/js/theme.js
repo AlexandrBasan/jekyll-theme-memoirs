2574e1cf21e361ea508a73a72c0e7e3ab250149c
Alexandr Basan <alexandr.basan@gmail.com>
2021-01-22 11:23:58 +1300

    scroll to top

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -164,6 +164,23 @@ $(document).ready(function(){
         return false;
     });
 
+    ////////////////////////////////////////
+
+    	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+
 });
 
 
